bc1a6c9953ca4363c745135ee97fcb180f03e5b9
LarsKue <lars@kuehmichel.de>
2021-08-03 16:04:28 +0200

    Added tensorboard for plots

--- pilot_experiments/covid-policy-tracker/main.py
+++ pilot_experiments/covid-policy-tracker/main.py
@@ -1,13 +1,21 @@
+import os
 
 from datamodule import ResponseDataModule
 from policy_tracker import PolicyTracker
 from trainer import trainer
+
+import subprocess as sub
+
+
+def tensorboard():
+    sub.run(["tensorboard", "--logdir", "lightning_logs"])
 
 
 def main():
     dm = ResponseDataModule()
     pt = PolicyTracker()
     trainer.fit(pt, dm)
+    tensorboard()
 
 
 if __name__ == "__main__":
