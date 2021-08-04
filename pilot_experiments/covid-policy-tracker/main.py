8c2adf6d80880bd280d6924c91f60cc3ec3a235b
NicolasWo <74407445+NicolasWo@users.noreply.github.com>
2021-08-04 16:06:13 +0200

    one-hot all countries, test prediction

--- pilot_experiments/covid-policy-tracker/main.py
+++ pilot_experiments/covid-policy-tracker/main.py
@@ -7,6 +7,13 @@ from trainer import trainer
 import subprocess as sub
 
 
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, TensorDataset, random_split
+import matplotlib.pyplot as plt
+
+
 def tensorboard():
     sub.run(["tensorboard", "--logdir", "lightning_logs"])
 
@@ -18,5 +25,33 @@ def main():
     tensorboard()
 
 
+def test():
+    model = PolicyTracker.load_from_checkpoint("./lightning_logs/version_0/checkpoints/epoch=200-step=2612.ckpt")
+    #model = PolicyTracker.load_from_checkpoint("./lightning_logs/version_1/checkpoints/epoch=199-step=382799.ckpt")
+
+
+    df = pd.read_csv("policies_onehot_Germany.csv")
+    y = df.pop("reproduction_rate").to_numpy()
+    x = df.to_numpy()
+
+    # drop index
+    x = x[:, 1:]
+
+    x = torch.Tensor(x)
+    y = torch.Tensor(y).unsqueeze(-1)
+
+    print(x.shape)
+    print(y.shape)
+
+
+    predicted = model(x)
+
+    plt.figure()
+    plt.plot(y)
+    plt.plot(predicted.detach().cpu().numpy())
+
+    plt.show()
+
 if __name__ == "__main__":
-    main()
+    #main()
+    test()
--- pilot_experiments/covid-policy-tracker/playground.py
+++ pilot_experiments/covid-policy-tracker/playground.py
@@ -6,8 +6,89 @@ import pandas as pd
 import torch
 
 
+def create_policies_oh(responses, testing, country_name):
+    print(country_name)
+
+    # filter for one country, for now
+    responses_single = responses[responses["CountryName"] == country_name].copy()
+    testing_sinlge = testing[testing["location"] == country_name].copy()
+
+    # superset of dates from start to finish
+    dates = pd.date_range(start=responses_single["Date"].min(), end=responses_single["Date"].max(), freq="1D")
+
+    # set the new dates as index for both dataframes
+    responses_single.set_index("Date", inplace=True)
+    responses_single = responses_single.reindex(dates)
+
+    testing_sinlge.set_index("date", inplace=True)
+    testing_sinlge = testing_sinlge.reindex(dates)
+
+    responses_single = responses_single.reset_index()
+    testing_sinlge = testing_sinlge.reset_index()
+
+    # TODO: Maybe don't interpolate, and instead just drop NaNs if they are only at start and end
+    # # interpolate the nan values using a piecewise constant function
+    # # TODO: linear interpolation is also feasible (method="linear")
+    # testing = testing.interpolate(method="pad")
+
+    policy_names = [
+        "C1_School closing",
+        "C2_Workplace closing",
+        "C3_Cancel public events",
+        "C4_Restrictions on gatherings",
+        "C5_Close public transport",
+        "C6_Stay at home requirements",
+        "C7_Restrictions on internal movement",
+        "C8_International travel controls",
+        "E1_Income support",
+        "E2_Debt/contract relief",
+        "H1_Public information campaigns",
+        "H2_Testing policy",
+        "H3_Contact tracing",
+        "H6_Facial Coverings",
+        "H7_Vaccination policy",
+        "H8_Protection of elderly people"
+    ]
+
+    policies = responses_single[policy_names]
+
+    max_values = [3, 3, 2, 4, 2, 3, 2, 4, 2, 2, 2, 3, 2, 4, 5, 3]
+    n = sum(max_values)
+
+    policies_oh_single = torch.zeros(len(policies), n)
+
+    for i, row in policies.iterrows():
+        oh = torch.zeros(n)
+        offset = 0
+
+        for j, index in enumerate(row):
+            try:
+                oh[offset + int(index) - 1] = 1.0
+            except ValueError:
+                # nan
+                pass
+
+            offset += max_values[j]
+
+        policies_oh_single[i] = oh
+
+    policies_oh_single = pd.DataFrame(policies_oh_single.numpy())
+
+    # relevant data from testing is in vaccinations and reproduction rate
+    policies_oh_single["vaccination_rate"] = testing_sinlge["people_fully_vaccinated_per_hundred"].fillna(0.0)
+    policies_oh_single["reproduction_rate"] = testing_sinlge["reproduction_rate"]
+
+    policies_oh_single = policies_oh_single.dropna()
+
+    return policies_oh_single
+
+
 responses = GitHubData(user="OxCGRT", repo="covid-policy-tracker", branch="master", path="data/OxCGRT_latest.csv")
 testing = GitHubData(user="owid", repo="covid-19-data", branch="master", path="public/data/owid-covid-data.csv")
+
+
+responses.save()
+testing.save()
 
 responses.load()
 testing.load()
@@ -21,71 +102,16 @@ testing["date"] = pd.to_datetime(testing["date"], format="%Y-%m-%d")
 print(responses.dtypes)
 print(testing.dtypes)
 
-# filter for one country, for now
-responses = responses[responses["CountryName"] == "Germany"]
-testing = testing[testing["location"] == "Germany"]
+print(responses["CountryName"].unique())
+print(testing["location"].unique())
+intersection = [x for x in responses["CountryName"].unique() if x in testing["location"].unique() and x not in ["Germany", "Brazil", "Canada", "China", "United Kingdom", "United States"]]
+print(intersection)
+policies_oh = create_policies_oh(responses, testing, intersection[0])
+print(policies_oh)
 
-# superset of dates from start to finish
-dates = pd.date_range(start=responses["Date"].min(), end=responses["Date"].max(), freq="1D")
+for country_name in intersection[1:]:
+    policies_oh = policies_oh.append(create_policies_oh(responses, testing, country_name))
 
-# set the new dates as index for both dataframes
-responses.set_index("Date", inplace=True)
-responses = responses.reindex(dates)
+policies_oh.to_csv("policies_onehot_full.csv")
 
-testing.set_index("date", inplace=True)
-testing = testing.reindex(dates)
-
-responses = responses.reset_index()
-testing = testing.reset_index()
-
-# TODO: Maybe don't interpolate, and instead just drop NaNs if they are only at start and end
-# # interpolate the nan values using a piecewise constant function
-# # TODO: linear interpolation is also feasible (method="linear")
-# testing = testing.interpolate(method="pad")
-
-policy_names = [
-    "C1_School closing",
-    "C2_Workplace closing",
-    "C3_Cancel public events",
-    "C4_Restrictions on gatherings",
-    "C5_Close public transport",
-    "C6_Stay at home requirements",
-    "C7_Restrictions on internal movement",
-    "C8_International travel controls",
-    "E1_Income support",
-    "E2_Debt/contract relief",
-    "H1_Public information campaigns",
-    "H2_Testing policy",
-    "H3_Contact tracing",
-    "H6_Facial Coverings",
-    "H7_Vaccination policy",
-    "H8_Protection of elderly people"
-]
-
-policies = responses[policy_names]
-
-max_values = [3, 3, 2, 4, 2, 3, 2, 4, 2, 2, 2, 3, 2, 4, 5, 3]
-n = sum(max_values)
-
-policies_oh = torch.zeros(len(policies), n)
-
-for i, row in policies.iterrows():
-    oh = torch.zeros(n)
-    offset = 0
-
-    for j, index in enumerate(row):
-        oh[offset + int(index) - 1] = 1.0
-
-        offset += max_values[j]
-
-    policies_oh[i] = oh
-
-policies_oh = pd.DataFrame(policies_oh.numpy())
-
-# relevant data from testing is in vaccinations and reproduction rate
-policies_oh["vaccination_rate"] = testing["people_fully_vaccinated_per_hundred"].fillna(0.0)
-policies_oh["reproduction_rate"] = testing["reproduction_rate"]
-
-policies_oh = policies_oh.dropna()
-
-policies_oh.to_csv("policies_onehot.csv")
+create_policies_oh(responses, testing, "Germany").to_csv("policies_onehot_Germany.csv")
